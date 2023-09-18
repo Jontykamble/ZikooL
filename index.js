7a8466d3fc430a9edfe4e7604cb525224d3be5bc
Jonty_jk <88649711+Jontykamble@users.noreply.github.com>
2023-09-18 23:51:07 +0530

    Add files via upload

--- index.js
+++ index.js
@@ -1,37 +1,17 @@
-const express = require('express')
-const app = express()
-const port = 5000
-const mongoDB=require("./db")
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+import App from './App';
+import reportWebVitals from './reportWebVitals';
 
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
 
-
-app.use((req,res,next)=>{
-  res.setHeader("Access-Control-Allow-Origin","http://localhost:3000");
-  res.header(
-    "Access-Control-Allow-Headers",
-    "Origin,X-Requested-with,Content-Type,Accept"
-  );
-  next();
-
-
-})
-
-
-app.use(express.json());
-app.get('/', (req, res) => {
-  res.send('Hello World! update')
-})
-app.use("/abcd",(req,res)=>{
-  res.send("Hello , its a new route")
-})
-const Displaydata = require('./routes/Displaydata');
-app.use('/api',require("./routes/CreateUser"))
-app.use('/api',require("./routes/Displaydata"))
-// app.use('/jk',require("./abc"))
-// app.get("/a1",(req,res)=>{
-//   res.send("Working");
-// })
-
-app.listen(process.env.PORT || 5001, () => {
-  console.log(`Example app listening on port 5001`)
-})
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+reportWebVitals();
